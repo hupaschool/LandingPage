ed1147f511a670f2d7cc9d3949d9acdde4b1a886
yow3l <schoolhupa@gmail.com>
2025-10-19 22:51:27 +0300

    added logos

--- src/landingPage.jsx
+++ src/landingPage.jsx
@@ -1,7 +1,12 @@
+import airForce from './assets/airforcesymbol.svg';
+import kanaf from "./assets/kanaf.svg";
+
 const LandingPage = () => {
     return ( 
-        <div className="test">
-            test
+        <div className="header">
+            <img className= "logo" id= "iaf-logo" src={airForce} alt="Air Force Symbol" />
+            <img className= "logo" id= "kanaf-logo" src={kanaf} alt="kanaf symbol" />
+
         </div>
      );
 }
